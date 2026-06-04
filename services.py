4fad17b4a67770fb769bb9cab2cf332b87935ac8
asegoviacornejo-lab <a.segovia.cornejo@gmail.com>
2026-06-04 08:40:35 -0400

    Add services.py with various function calls

---
+++ services.py
@@ -0,0 +1,16 @@
+from datetime import date
+from datetime import datetime
+
+from database import (
+    connect,
+    fetch_all,
+    fetch_one,
+)
+active_semester_id()
+subjects()
+subject_options()
+grade_average()
+subject_progress()
+setup_progress()
+recommendation()
+risk_label()
