8883c768ee1ab15dec491d0f0e251089eb8add56
asegoviacornejo-lab <a.segovia.cornejo@gmail.com>
2026-06-04 08:38:57 -0400

    Add authentication functions in auth.py

---
+++ auth.py
@@ -0,0 +1,12 @@
+import hashlib
+import os
+
+from datetime import date
+from datetime import datetime
+
+from database import connect
+from database import fetch_one
+hash_password()
+verify_password()
+create_user()
+authenticate()
